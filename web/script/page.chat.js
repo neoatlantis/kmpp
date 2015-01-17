f9283daee1e4357a6c6ca2dfc976a114ede667af
neoatlantis <rijndael@yeah.net>
2015-01-17 03:24:41 +0100

    doing ui

--- web/script/page.chat.js
+++ web/script/page.chat.js
@@ -2,13 +2,27 @@ var buddyJID, roster;
 
 //////////////////////////////////////////////////////////////////////////////
 
-
 function onRosterRefresh(){
     roster = JSON.parse(localStorage.getItem('roster'));
     var buddyEntry = roster[buddyJID] || {};
     var buddyName = buddyEntry.name || '';
     $('.buddyName').text(buddyName);
     $('.buddyJID').text(buddyJID);
+};
+
+function changePageSafetyImplication(safe){
+    $('textarea,body,.chat-history').addClass(
+        (safe?'implicit-safe':'implicit-unsafe')
+    );
+};
+
+function onResize(){
+    $('.chat-history')
+        .css(
+            'height',
+            ($(window).height() - $('.chat-box-container').height()) + 'px'
+        )
+    ;
 };
 
 //////////////////////////////////////////////////////////////////////////////
@@ -24,7 +38,11 @@ require([
 
     buddyJID = kmpp.page.hashtag();
 
+    $(window).on('resize', onResize);
+    onResize();
+
     kmpp.page.on('update.xmpp.roster.refresh', onRosterRefresh);
     onRosterRefresh();
 
+    changePageSafetyImplication(false);
 }); });
