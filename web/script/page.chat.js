c4a2c129330a51057aae783b35b0ea919a49a67b
neoatlantis <rijndael@yeah.net>
2015-01-17 01:45:05 +0100

    doing chat dialog

--- web/script/page.chat.js
+++ web/script/page.chat.js
@@ -1,3 +1,18 @@
+var buddyJID, roster;
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+function onRosterRefresh(){
+    roster = JSON.parse(localStorage.getItem('roster'));
+    var buddyEntry = roster[buddyJID] || {};
+    var buddyName = buddyEntry.name || '';
+    $('.buddyName').text(buddyName);
+    $('.buddyJID').text(buddyJID);
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
 require([
     'jquery',
     'kmpp',
@@ -7,5 +22,9 @@ require([
 ){ $(function(){
     if(!kmpp.session.isLoggedIn()) return kmpp.page.redirect('/login/');
 
-    
+    buddyJID = kmpp.page.hashtag();
+
+    kmpp.page.on('update.xmpp.roster.refresh', onRosterRefresh);
+    onRosterRefresh();
+
 }); });
